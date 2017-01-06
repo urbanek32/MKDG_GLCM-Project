0ddf72bcc0afdea8416e4ef939547caa51a673ef
Michal Zylowski <zylowskimichal@gmail.com>
2017-01-06 13:50:43 +0100

    Showing results in Excel

--- GLCM-Magic/MainWindow.xaml.cs
+++ GLCM-Magic/MainWindow.xaml.cs
@@ -17,6 +17,8 @@ using System.Windows.Shapes;
 using Accord.Imaging;
 using Accord.Statistics;
 using Microsoft.Win32;
+using Excel=Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace GLCM_Magic
 {
@@ -97,6 +99,68 @@ namespace GLCM_Magic
             correlationLabel.Content = string.Format("Correlation: {0}", haralick.Correlation.ToString("N"));
             invDiffMomentLabel.Content = string.Format("Inv Diff Moment: {0}", haralick.InverseDifferenceMoment.ToString("N"));
             contrast.Content = string.Format("Contrast: {0}", haralick.Contrast.ToString("N"));
+
+            if (excelCheckBox.IsChecked != null)
+            {
+                if(excelCheckBox.IsChecked.Value == true)
+                showResultsInExcel(results);
+            }
+        }
+
+        private void showResultsInExcel(double[,] results)
+        {
+            Excel.Application oXL;
+            Excel._Workbook oWB;
+            Excel._Worksheet oSheet;
+            Excel.Range oRng;
+            try
+            {
+                oXL = new Excel.Application();
+                oXL.Visible = true;
+
+                oWB = (Excel._Workbook)(oXL.Workbooks.Add(Missing.Value));
+                oSheet = (Excel._Worksheet)oWB.ActiveSheet;
+
+                for (int i = 1; i < results.GetLength(0); i++)
+                {
+                    oSheet.Cells[1, i] = i;
+                    oSheet.Cells[i, 1] = i;
+                }
+
+                //bold, vertical alignment = center.
+                oSheet.get_Range("A1", "IV1").Font.Bold = true;
+                oSheet.get_Range("A1", "IV1").VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+                oSheet.get_Range("A1", "A256").Font.Bold = true;
+                oSheet.get_Range("A1", "A256").VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+
+                // Create an array to multiple values at once.
+                string[,] excelValues = new string[256, 256];
+
+                for (int i = 0; i < results.GetLength(0); i++)
+                {
+                    for (int j = 0; j < results.GetLength(0); j++)
+                    {
+                        excelValues[i, j] = results[i, j].ToString(); //TODO: Some prettier float formatting
+                    }
+                }
+
+                oSheet.get_Range("A2", "IV256").Value2 = excelValues;
+
+                oRng = oSheet.get_Range("A2", "IV256");
+                oRng.EntireColumn.AutoFit();
+
+                oXL.Visible = true;
+                oXL.UserControl = true;
+            }
+            catch (Exception theException)
+            {
+                String errorMessage;
+                errorMessage = "Error: ";
+                errorMessage = String.Concat(errorMessage, theException.Message);
+                errorMessage = String.Concat(errorMessage, " Line: ");
+                errorMessage = String.Concat(errorMessage, theException.Source);
+                MessageBox.Show(errorMessage, "Error");
+            }
         }
     }
 }
